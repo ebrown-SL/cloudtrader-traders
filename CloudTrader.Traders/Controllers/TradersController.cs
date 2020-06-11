d4d00649c55828bf9e453d87fe920f555ef728ed
Tilly Woodfield <tillyrwoodfield@gmail.com>
2020-06-11 11:11:13 +0100

    Remove authorization and update controller

---
+++ CloudTrader.Traders/Controllers/TradersController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CloudtraderTraders.Models;
+using CloudtraderTraders.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudtraderTraders.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TradersController : ControllerBase
+    {
+        private readonly ITraderService _traderService;
+
+        public TradersController(ITraderService traderService)
+        {
+            _traderService = traderService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TraderModel>>> GetAll()
+        {
+            var traders = await _traderService.GetAll();
+
+            return Ok(traders);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TraderModel>> GetTrader(int id)
+        {
+            var trader = await _traderService.GetById(id);
+
+            if (trader == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(trader);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TraderModel>> PostTrader(TraderModel traderModel)
+        {
+            var trader = await _traderService.Create(traderModel);
+
+            return Ok(trader);
+        }
+    }
+}
--- CloudTrader.Traders/Startup.cs
+++ CloudTrader.Traders/Startup.cs
@@ -28,51 +28,15 @@ namespace CloudtraderTraders
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase(databaseName: "Traders"));
+
             services.AddControllers();
 
-            // DI services
             services.AddScoped<ITraderService, TraderService>();
-
-            // auth (currently only encoding/verifying against trader's id into JWT)
-            var key = Encoding.ASCII.GetBytes("SOMESECRETTHATSHOULDNOTBEHARDCODEDFORVERIFYINGAUTHENTICITYOFJWTTOKENS");
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
-            {
-                x.Events = new JwtBearerEvents
-                {
-                    OnTokenValidated = context =>
-                    {
-                        var userService = context.HttpContext.RequestServices.GetRequiredService<ITraderService>();
-                        var userId = int.Parse(context.Principal.Identity.Name);
-                        var user = userService.GetById(userId);
-                        if (user == null)
-                        {
-                            // return unauthorized if user no longer exists
-                            context.Fail("Unauthorized");
-                        }
-                        return Task.CompletedTask;
-                    }
-                };
-                x.RequireHttpsMetadata = true;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -80,12 +44,8 @@ namespace CloudtraderTraders
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
             app.UseRouting();
 
-            //the order of these middleware here matters
-            app.UseAuthentication();
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
