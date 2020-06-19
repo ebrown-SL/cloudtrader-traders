150a10319a2c82cb9002ed71cad667682ef14e6d
Tilly Woodfield <tillyrwoodfield@gmail.com>
2020-06-19 12:28:31 +0100

    Add data layer tests

---
+++ test/CloudTrader.Traders.Data.Tests/TraderDbModelTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using NUnit.Framework;
+
+namespace CloudTrader.Traders.Data.Tests
+{
+    public class TraderDbModelTests
+    {
+        [TestCase(null)]
+        public void Id_CantBeNull(int id)
+        {
+            var trader = new TraderDbModel
+            {
+                Id = id,
+                Balance = 100
+            };
+
+            Assert.IsNotNull(trader.Id);
+        }
+
+        [TestCase(null)]
+        public void Balance_CantBeNull(int balance)
+        {
+            var trader = new TraderDbModel
+            {
+                Id = 1,
+                Balance = balance
+            };
+
+            Assert.IsNotNull(trader.Balance);
+        }
+
+        [Test]
+        public void Balance_Zero_IsValid()
+        {
+            var trader = new TraderDbModel
+            {
+                Id = 1,
+                Balance = 0
+            };
+
+            var validationResults = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(trader, new ValidationContext(trader), validationResults);
+
+            Assert.True(isValid);
+        }
+    }
+}
