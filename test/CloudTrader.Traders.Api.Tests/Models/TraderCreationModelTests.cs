6a785f99f59e32d5cea58e151de89d6b654e0b9f
Tilly Woodfield <tillyrwoodfield@gmail.com>
2020-06-19 12:25:44 +0100

    Add Traders.Api tests

---
+++ test/CloudTrader.Traders.Api.Tests/Models/TraderCreationModelTests.cs
@@ -0,0 +1,19 @@
+﻿using CloudTrader.Traders.Api.Models;
+using NUnit.Framework;
+
+namespace CloudTrader.Traders.Api.Tests.Models
+{
+    public class TraderCreationModelTests
+    {
+        [TestCase(null)]
+        public void TraderCreationModel_Id_CantBeNull(int id)
+        {
+            var trader = new TraderCreationModel
+            {
+                Id = id
+            };
+
+            Assert.IsNotNull(trader.Id);
+        }
+    }
+}
