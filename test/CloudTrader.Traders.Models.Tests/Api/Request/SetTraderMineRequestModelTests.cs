4bf755d4797e028474e4480413e7e096c8873cf0
Eddy Brown <eddybrown@me.com>
2020-07-23 11:37:35 +0100

    Update tests

---
+++ test/CloudTrader.Traders.Models.Tests/Api/Request/SetTraderMineRequestModelTests.cs
@@ -0,0 +1,33 @@
+﻿using CloudTrader.Traders.Models.Api.Request;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CloudTrader.Traders.Models.Tests.Api.Request
+{
+    public class SetTraderMineRequestModelTests
+    {
+
+        Guid guid = new Guid();
+
+        [TestCase(0, ExpectedResult = true)]
+        [TestCase(-1, ExpectedResult = false)]
+        [TestCase(100, ExpectedResult = true)]
+        [TestCase(-5, ExpectedResult = false)]
+        public bool Stock_NegativeValue_IsNotValid_AllOthersValid(int stock)
+        {
+            var model = new SetTraderMineRequestModel
+            {
+                MineId = guid,
+                Stock = stock
+            };
+
+            var result = new List<ValidationResult>();
+            var validationContext = new ValidationContext(model);
+            var isValid = Validator.TryValidateObject(model, validationContext, result, true);
+
+            return isValid;
+        }
+    }
+}
